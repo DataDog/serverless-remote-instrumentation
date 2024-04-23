dbbcb2001bc0ba6139175c0617fe530185ee0864
kimi <kimi.wu@datadoghq.com>
2024-04-23 12:48:02 -0400

    add

--- scripts/build_layer.sh
+++ scripts/build_layer.sh
@@ -6,17 +6,18 @@
 # Copyright 2024 Datadog, Inc.
 
 # This script is only tested on MacBook Pro M1
-# nodejs is the designated directory specified in Lambda documentation
 
 rm -rf node_modules
 rm -rf scripts/.layers
 yarn install
 mkdir -p scripts/.layers
+
+# nodejs is the designated directory specified in Lambda documentation
 mkdir -p nodejs
 cp -r node_modules nodejs/
 mkdir -p nodejs/node_modules/datadog-remote-instrument
-#echo '{"version": "1.0.0","dependencies": {},"name":"datadog-remote-instrument","main": "handler.js"}' >> nodejs/node_modules/datadog-remote-instrument/package.json
-# need to put handler into node_modules/ as a package
+
+# put handler.js into node_modules/package-name/ as a package
 cp handler.js nodejs/node_modules/datadog-remote-instrument/handler.js
 zip -r scripts/.layers/datadog_serverless_remote_instrumentation_arm64.zip nodejs
-#rm -rf nodejs
+rm -rf nodejs
