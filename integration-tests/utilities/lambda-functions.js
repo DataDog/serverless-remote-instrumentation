625be5705b1bf2f65e6423c0a69688f601c93687
Tal Usvyatsky <tal.usvyatsky@datadoghq.com>
2025-01-28 13:24:08 -0500

    Default runtime back to node

--- integration-tests/utilities/lambda-functions.js
+++ integration-tests/utilities/lambda-functions.js
@@ -5,6 +5,7 @@ const {
   GetFunctionConfigurationCommand,
   ResourceNotFoundException,
   TagResourceCommand,
+  Runtime,
 } = require("@aws-sdk/client-lambda");
 const { account, region, testLambdaRole } = require("../config.json");
 const { getLambdaClient } = require("./aws-resources");
@@ -25,6 +26,7 @@ const createFunction = async (lambdaProps) => {
     },
     Handler: "index.handler",
     Role: `arn:aws:iam::${account}:role/${testLambdaRole}`,
+    Runtime: Runtime.nodejs20x,
     PackageType: "Zip",
     MemorySize: 512,
     ...lambdaProps,
