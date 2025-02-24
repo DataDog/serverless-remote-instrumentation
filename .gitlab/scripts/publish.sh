d1552888f09436494cf7204a3e012fd6403415ba
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2025-02-24 17:56:38 -0500

    fix issue with script location

--- .gitlab/scripts/publish.sh
+++ .gitlab/scripts/publish.sh
@@ -24,7 +24,7 @@ fi
 
 # Move into the root directory
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $SCRIPTS_DIR/..
+cd $SCRIPTS_DIR/../..
 
 VERSION=$VERSION ARCHITECTURE=$ARCHITECTURE ./scripts/build_layer.sh
 
