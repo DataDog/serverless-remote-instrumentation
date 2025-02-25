ee9279545ce77931ea65b463f711f49b7f74f06f
jordan gonzález <30836115+duncanista@users.noreply.github.com>
2025-02-24 22:16:02 -0500

    update script to publish without asking for confirmation

--- scripts/publish_layers.sh
+++ scripts/publish_layers.sh
@@ -116,11 +116,7 @@ do
             echo "Layer $layer_name  version $VERSION already exists in region $region, skipping..."
             continue
         elif [ $latest_version -lt $((VERSION-1)) ]; then
-            read -p "WARNING: The latest version of layer $layer_name in region $region is $latest_version, publish all the missing versions including $VERSION or EXIT the script (y/n)?" CONT
-            if [ "$CONT" != "y" ]; then
-                echo "Exiting"
-                exit 1
-            fi
+            echo "WARNING: The latest version of layer $layer_name in region $region is $latest_version, publishing all the missing versions including $VERSION"
         fi
         index=$(index_of_layer $layer_name)
         layer_path="${LAYER_PATHS[$index]}"
