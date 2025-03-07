a2aef370d5cb6a11f2364f83e1860dd010c676f1
Alex Angelillo <alex.angelillo@datadoghq.com>
2025-03-07 10:36:22 -0500

    Bump up integration test timeout
    Increase the test timeout since I've observed a couple of the tests taking
    like 65 seconds with the increased timeout.

--- integration-tests/jest.config.js
+++ integration-tests/jest.config.js
@@ -1,7 +1,7 @@
 const config = {
   globalTeardown: "./post-test-validation.js",
   testMatch: ["**/integration-tests/**+(test|spec).[jt]s?(x)"],
-  testTimeout: 60000, // ms
+  testTimeout: 90000, // ms
 };
 
 module.exports = config;
