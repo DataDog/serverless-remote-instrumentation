adb2d5877b8dbd8f967c2d67e49a7e5050e9df45
Alex Angelillo <alex.angelillo@datadoghq.com>
2025-03-26 16:07:03 -0400

    Pull site and secret names to variables
    Pull the site and secret names to variables.  Originally I was going to
    change them from staging (datad0g) to ddstaging but that doesn't work
    yet because of the permissions for remote config.  So I kept the values
    the same but pulled them out like this to be easier to change when we
    want to do that in the future.

--- integration-tests/utilities/datadog-keys.js
+++ integration-tests/utilities/datadog-keys.js
@@ -1,5 +1,6 @@
 const { GetSecretValueCommand } = require("@aws-sdk/client-secrets-manager");
 const { getSecretsManagerClient } = require("./aws-resources");
+const { apiSecretName, appSecretName } = require("../config.json");
 
 const getSecret = async (name) => {
   const secretsManager = await getSecretsManagerClient();
@@ -12,13 +13,13 @@ const getSecret = async (name) => {
 };
 
 const getApiKey = async () => {
-  return getSecret("Remote_Instrumenter_Test_API_Key_20250226");
+  return getSecret(apiSecretName);
 };
 
 exports.getApiKey = getApiKey;
 
 const getAppKey = async () => {
-  return getSecret("Remote_Instrumenter_Test_APPLICATION_Key");
+  return getSecret(appSecretName);
 };
 
 exports.getAppKey = getAppKey;
