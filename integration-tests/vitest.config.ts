6bb17dc6dd80406522ec06d4a7b195e720a854d7
Ava Silver <ava.silver@datadoghq.com>
2026-03-18 16:04:40 +0100

    [SVLS-7950] fix integration tests

--- integration-tests/vitest.config.ts
+++ integration-tests/vitest.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
     include: ["integration-tests/**/*.test.ts"],
     testTimeout: 90_000,
     retry: process.env.CI ? 3 : 0,
+    fileParallelism: false,
     globalSetup: ["./integration-tests/vitest-global-setup.ts"],
   },
 });
